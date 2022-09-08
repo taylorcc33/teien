6e56008da7ae7ddb8cbc549b9301464c2ca63671
Taylor Collins <taylorcc33@gmail.com>
2022-09-07 23:11:57 -0600

    updates

--- lib/teien.rb
+++ lib/teien.rb
@@ -1,5 +1,21 @@
 class Teien
-  def gen
+  # get path to markdown directory
+  # load markdown files from directory
+  #  
 
+
+  def gen
+    markdown_files
+  end
+
+  def markdown_path
+    path = 'markdown' if Dir.exist?('markdown')
+  end
+
+  def markdown_files
+    files = Dir.entries(markdown_path)
+    files.delete(".")
+    files.delete("..")
+    files
   end
 end
