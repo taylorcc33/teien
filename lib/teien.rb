1da9d336c00176041e0d2948adfa583bff3f476e
Taylor Collins <taylorcc33@gmail.com>
2022-12-10 20:15:58 -0700

    remove unneeded code

--- lib/teien.rb
+++ lib/teien.rb
@@ -6,59 +6,5 @@ require_relative 'teien/generator'
 require_relative 'teien/interpreter'
 # top level documentation comment
 module Teien
-  # ✅ get path to markdown directory
-  # ✅ load markdown file from directory
-  # ✅ parse frontmatter from file
-  # ✅ parse html body from file
-  # -  add html to template in dist folder
-  # -  get html template
-  # -  sub out content tag with parsed html content.
-  #
-  #
-  #
-  #
-  # loop to be handle multiple .md files
 
-  # in Parser
-  def generate_html_files
-    files = markdown_files
-    parsed_files = []
-
-    files.each do |file|
-      parsed = parse(file)
-      parsed_files << parsed
-    end
-
-    # meta_data = parsed.front_matter
-
-    # html_body = parse_body(parsed.content)
-  end
-
-  # in Parser
-  def markdown_files
-    files = Dir.entries(markdown_path)
-    files.delete('.')
-    files.delete('..')
-    files
-  end
-
-  # in Parser
-  def markdown_path
-    path = 'markdown' if Dir.exist?('markdown')
-  end
-
-  # in Parser
-  def parse(file)
-    parsed = FrontMatterParser::Parser.parse_file("#{markdown_path}/#{file}")
-  end
-
-  def parse_body(content)
-    renderer = Redcarpet::Render::HTML.new
-    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
-    markdown.render(content)
-  end
-
-  def html_template
-    template = File.read("templates/basic_temp.html")
-  end
 end
