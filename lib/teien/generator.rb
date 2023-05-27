5e2847d3bf9d94c063540b1ff253251130b54b57
Taylor Collins <taylorcc33@gmail.com>
2023-05-27 15:12:52 -0600

    add dynamic navbar generation

--- lib/teien/generator.rb
+++ lib/teien/generator.rb
@@ -1,13 +1,16 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 module Teien
   # Generates the html files in the dist folder after the parsed markdown is interpreted into html.
 
   class Generator
     attr_reader :html_template, :navbar
+    attr_accessor :navbar_list
 
     def initialize
       @html_template = File.read('templates/basic_temp.html')
+
+      @navbar_list = ""
 
       # @navbar = {
       #   "dev": {
@@ -40,10 +43,15 @@ module Teien
     def generate_html_files(files)
       delete_html_files
 
+      # Loop over files and add navbar list items to navbar_list
+      create_navbar_list(files)
+
+      # Loop over files and generate html files from file[:content] and navbar_list
       html_files = files.map do |file|
         new_html = html_template.gsub(/<!-- CONTENT -->/, file[:content])
         new_html = new_html.gsub(/<!-- TITLE -->/, file[:front_matter]['title'])
-        # new_html = new_html.gsub(/<!-- NAV -->/, generate_navbar)
+        new_html = new_html.gsub(/<!-- NAV -->/, generate_navbar(navbar_list))
+        file_name = ""
 
         if file[:front_matter]['file_name']
           file_name = file[:front_matter]['file_name']
@@ -51,28 +59,41 @@ module Teien
           file_name = file[:front_matter]['title'].downcase.split(' ').join('-')
         end
 
-        nav_location ||= file[:front_matter]['nav_location']
-
         File.open("dist/#{file_name}.html", 'w') { |f| f.write new_html }
       end
     end
 
     private   
 
+    # Clears out old html files before generating new ones
     def delete_html_files
       Dir['dist/*.html'].each do |file|
         File.delete(file)
       end
     end
 
+    def create_navbar_list(files)
+      files.map do |file|
+        nav_item = file[:front_matter]['title']
+        
+        if file[:front_matter]['file_name']
+          nav_link = file[:front_matter]['file_name']
+        else
+          nav_link = file[:front_matter]['title'].downcase.split(' ').join('-')
+        end
+
+        navbar_list << "<li><a href=\"#{nav_link}.html\">#{nav_item}</a></li>"
+      end
+    end
+
     # TODO: generate dynamic navbar like 100r site nav
-    def generate_navbar
+    def generate_navbar(navbar_list)
       navbar = <<-HEREDOC
       <summary>Menu</summary>
       <section class="site-nav">
         <section>
           <ul class="nobull capital">
-            <li><a href="
+            #{navbar_list}
           </ul>
         </section>
       </section>
@@ -81,9 +102,3 @@ module Teien
     end
   end
 end
-
-# Load html_template in initializer - DONE!
-# Map over files - DONE!
-# Insert content into html_template body - DONE!
-# Insert frontmatter into relevant header fields - DONE!
-# Save html file into relevant dist directory
---
+++ markdown/taylor.md
@@ -0,0 +1,8 @@
+---
+title: Taylor
+nav_location: navbar
+author: Taylor Collins
+data: 09-10-2022
+---
+
+# My name is taylor
