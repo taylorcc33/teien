0c645a5d11a847bd2a6dc760cda7dd5e6af0aa3f
Taylor Collins <taylorcc33@gmail.com>
2024-03-03 17:42:24 -0700

    add gitignore

--- lib/teien.rb
+++ lib/teien.rb
@@ -6,15 +6,3 @@ require_relative 'teien/parser'
 require_relative 'teien/app'
 require_relative 'teien/generator'
 require_relative 'teien/interpreter'
-
-# comment from test1
-
-# comment from test2
-
-# comment from test3
-
-# comment from test4
-
-def hello_world
-  puts 'hello world!'
-end
