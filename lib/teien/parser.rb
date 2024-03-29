10722e6c9d494e5ccf39fc0d7c54e5f165991d10
Taylor Collins <taylorcc33@gmail.com>
2024-03-29 15:03:43 -0600

    formatting

--- lib/teien/parser.rb
+++ lib/teien/parser.rb
@@ -4,11 +4,12 @@ module Teien
   # Parses markdown files with FrontMatterParser into array of FrontMatterParser objects:
   #
   # <FrontMatterParser::Parsed:0x00007f97a408b180
-  # @content="# Second h1 tag\n\n## Second h2 tag\n\nThis is some **bold text**.\n\n1. Ordered item 1\n2. Ordered item 2\n3. Ordered item 3\n\n```\n# here is some code\ndef test\n  p \"test\"\nend\n```\n",
+  # @content="# Second h1 tag\n\n## Second h2 tag\n\nThis is some **bold text**.\n\n1.
+  # Ordered item 1\n2. Ordered item 2\n3.
+  # Ordered item 3\n\n```\n# here is some code\ndef test\n  p \"test\"\nend\n```\n",
   # @front_matter={"title"=>"Second Test Page", "author"=>"Taylor Collins", "date"=>"09-10-2022"}>
   #
   # Access @content and @front_matter with . methods -> parsed_files[0].content
-
   class Parser
     attr_accessor :files, :parsed_files
 
